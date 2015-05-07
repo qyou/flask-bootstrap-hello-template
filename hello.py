37b8585b8c0627dbf62da1474d6c9553f2173142
qyou <qyou.casia@gmail.com>
2015-05-07 18:37:26 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import url_for
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", title="Hello")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
